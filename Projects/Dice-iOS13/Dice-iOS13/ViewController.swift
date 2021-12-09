b4722bd32e2e00fe25bf727c22aaed291d8a4ed9
Cahue Kokeny <cahue.kokeny@hotmail.com>
2021-12-09 08:37:13 -0500

    Updated Dice-iOS13 file

--- Projects/Dice-iOS13/Dice-iOS13/ViewController.swift
+++ Projects/Dice-iOS13/Dice-iOS13/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        diceImageView1.image = UIImage(named: "DiceSix")
+        diceImageView1.alpha = 0.5
+    
     }
 
-
+    @IBAction func rollDiceButton(_ sender: UIButton) {
+        
+        diceImageView1.image = UIImage(named: "DiceFour")
+        diceImageView1.alpha = 1
+        diceImageView2.image = UIImage(named: "DiceFour")
+    }
+    
 }
 
