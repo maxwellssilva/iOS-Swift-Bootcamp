65f350c5605fac785e8252c3890a66a6ebf11ebe
Cahue Kokeny <cahue.kokeny@hotmail.com>
2021-12-10 09:57:43 -0500

    Updated Challenge files

--- Challenges/challengeIMC/challengeIMC/ViewController.swift
+++ Challenges/challengeIMC/challengeIMC/ViewController.swift
@@ -12,30 +12,48 @@ class ViewController: UIViewController {
     @IBOutlet weak var alturaView: UITextField!
     @IBOutlet weak var pesoView: UITextField!
     @IBOutlet weak var textView1: UITextView!
+    @IBOutlet weak var imageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        imageView.isHidden = true
+        
        
     }
     
     @IBAction func btnGo(_ sender: UIButton) {
         
-        calcularImc(alturaCalc: alturaView.text!, pesoCalc: pesoView.text!)
+        calcImc(alt: alturaView.text!, pes: pesoView.text!)
         
-        textView1.text = String(imc)
-        
+    
     }
     
-    func calcularImc(alturaCalc: String, pesoCalc: String) {
+    func calcImc(alt: String, pes: String) -> Float {
         
-        let altura = Float()
-        let peso = Float()
-        var imc = Float()
+        let altura = alt
+        let peso = pes
+        let imc : Float = Float(peso)! / (Float(altura)! * Float(altura)!)
         
-        imc = peso / altura
         
-      
+        if imc < 18.5 {
+            imageView.isHidden = false
+            imageView.image = UIImage(named: "dededede")
+            textView1.text = "Seu IMC eh \(String(imc)). Voce esta abaixo do peso"
+        } else if imc > 24.9 {
+            imageView.isHidden = false
+            imageView.image = UIImage(named: "lulu")
+            textView1.text = "Seu IMC eh \(String(imc)). Voce esta acima do peso"
+        } else {
+            imageView.isHidden = false
+            imageView.image = UIImage(named: "download")
+            textView1.text = "Seu IMC eh \(String(imc)). Voce esta com peso normal"
+        }
+        
+        return imc
+        
     }
+  
     
     
 }
